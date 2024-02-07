873efa2f4b548e310fc560e7054a1048ce1ad527
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-02-07 15:05:38 +0000

    <p> refactor

--- client/src/components/HomeSections/FileActions.jsx
+++ client/src/components/HomeSections/FileActions.jsx
@@ -12,38 +12,54 @@ const FileActions = ({handleOpenPopup, selectedUser, selectedFile}) => {
     }
 
     const handleDownload = async () => {
-        try {
-            // Gets the file from IPFS
-            const fileContent = await FileHandler.getFileFromIPFS(selectedFile.ipfsCID);
-            console.log("Accessed file in IPFS.");
-
-            // Decrypts the file
-            const decryptedFileBuffer = await FileHandler.decryptFileWithSymmetricKey(storeFileContract, selectedFile, selectedUser, fileContent);
-            const blob = new Blob([decryptedFileBuffer]);
-            console.log("File Decrypted.");
-            
-            // Creates a downloaded link 
-            const downloadLink = document.createElement("a");
-            downloadLink.href = URL.createObjectURL(blob);
-            downloadLink.download = selectedFile.fileName;
-            document.body.appendChild(downloadLink);
-            downloadLink.click();
-            document.body.removeChild(downloadLink);
-        } catch (error) {
-            console.error("Error decrypting or downloading file: ", error);
+        if (selectedFile === null) {
+            console.log("Please select a file");
+        } else {
+            try {
+                // Gets the file from IPFS
+                const fileContent = await FileHandler.getFileFromIPFS(selectedFile.ipfsCID);
+                console.log("Accessed file in IPFS.");
+    
+                // Decrypts the file
+                const decryptedFileBuffer = await FileHandler.decryptFileWithSymmetricKey(storeFileContract, selectedFile, selectedUser, fileContent);
+                const blob = new Blob([decryptedFileBuffer]);
+                console.log("File Decrypted.");
+                
+                // Creates a downloaded link 
+                const downloadLink = document.createElement("a");
+                downloadLink.href = URL.createObjectURL(blob);
+                downloadLink.download = selectedFile.fileName;
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+                document.body.removeChild(downloadLink);
+            } catch (error) {
+                console.error("Error decrypting or downloading file: ", error);
+            }
         }
     }
 
     const handlePopupOpenDelete = () => {
-        handleOpenPopup("delte"); // TODO: PUT THIS AS A VARIABLE READ FROM ANOTHER PLACE
+        if (selectedFile !== null) {
+            handleOpenPopup("delte"); // TODO: PUT THIS AS A VARIABLE READ FROM ANOTHER PLACE
+        } else {
+            console.log("Please select a file");
+        }
     }
 
     const handlePopupOpenShare = () => {
-        handleOpenPopup("share"); // TODO: PUT THIS AS A VARIABLE READ FROM ANOTHER PLACE
+        if (selectedFile !== null) {
+            handleOpenPopup("share"); // TODO: PUT THIS AS A VARIABLE READ FROM ANOTHER PLACE
+        } else {
+            console.log("Please select a file");
+        }
     }
 
     const handlePopupOpenVerify = () => {
-        handleOpenPopup("verify"); // TODO: PUT THIS AS A VARIABLE READ FROM ANOTHER PLACE
+        if (selectedFile !== null) {
+            handleOpenPopup("verify"); // TODO: PUT THIS AS A VARIABLE READ FROM ANOTHER PLACE
+        } else {
+            console.log("Please select a file");
+        }
     }
 
     return (
