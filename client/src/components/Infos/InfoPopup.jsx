744ab76706e4424ef487e8e6a8340b8df5dd16f0
Jenifer Constantino <jeniferg00constantino@hotmail.com>
2024-03-26 16:58:06 +0000

    blurred interface when popup mnemonic

--- client/src/components/Infos/InfoPopup.jsx
+++ client/src/components/Infos/InfoPopup.jsx
@@ -5,27 +5,33 @@ const InfoPopup = ({handleContinue, message, title, showInfoPopup, iconComponent
 
     return (
         <div className={showHideClassName}>
-            <div className="popup-content">
-                <div className="right-section">
-                    <div className="content">
-                        {IconComponentnent  && <IconComponentnent size={80}/>}
-                        <p className="textInfo">{title}</p>
-                    </div>
-                </div>
-                <div className="buttons-content">
-                    <div>
-                        {mnemonic !== '' ? (
-                            <>
-                            <p className="message-popup"> This is your <b>mnemonic</b> </p>
-                            <p className="message-popup"> {mnemonic}</p>
-                            <p className="message-popup"> Keep it safe (write it on a peper, for example). Anyone with access to this mnemonic can <b>access your account</b>. And, if you lose it you <b>lose</b> your access forever.</p>
-                            </>
-                        ) : (
-                            <p className="message-popup">{message}</p>
-                        )}
-                    </div>
-                    <div>
-                        <button className="app-button app-button__popup" onClick={handleContinue}>Continue</button>
+
+            <div className='modal-wrapper'>
+                <div className="modal-background"></div>
+                <div className="modal modal-info-popup"> 
+                    <div className="popup-content">
+                        <div className="right-section">
+                            <div className="content">
+                                {IconComponentnent  && <IconComponentnent size={80}/>}
+                                <p className="textInfo">{title}</p>
+                            </div>
+                        </div>
+                        <div className="buttons-content">
+                            <div>
+                                {mnemonic !== '' ? (
+                                    <>
+                                    <p className="message-popup"> This is your <b>mnemonic</b> </p>
+                                    <p className="message-popup"> {mnemonic}</p>
+                                    <p className="message-popup"> Keep it safe (write it on a peper, for example). Anyone with access to this mnemonic can <b>access your account</b>. And, if you lose it you <b>lose</b> your access forever.</p>
+                                    </>
+                                ) : (
+                                    <p className="message-popup">{message}</p>
+                                )}
+                            </div>
+                            <div>
+                                <button className="app-button app-button__popup" onClick={handleContinue}>Continue</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
